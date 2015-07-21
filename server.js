36f935df5f81aa033ed053bcd31196cf752314f5
Felice DeNigris <felideni@gmail.com>
2015-07-20 21:00:41 -0400

    modified server

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var app = express();
 var path = require('path');
 
 
+
+app.set('port', (process.env.PORT|| 6060));
+app.use(express.static(__dirname));
+
+var server = app.listen(app.get('port'));
+
+//var port = process.env.PORT || 6060;
+
+/*
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname +'/index.html'));
 });
@@ -12,3 +21,5 @@ app.get('/', function(req, res){
 app.listen(6060, function(){
   console.log("listening on port 6060");
 });
+
+*/
