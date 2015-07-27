45f9a6d5094287f1d9f874ea4443e21aa6703446
Felice DeNigris <felideni@gmail.com>
2015-07-26 20:27:10 -0400

    Cleaned up code

--- app/js/Module/myapp.js
+++ app/js/Module/myapp.js
@@ -1,82 +1,82 @@
 // refactor the variable names to be written likeThis
 
 
- var myapp = angular.module('MyApp', []);
+ var myApp = angular.module('myApp', []);
 
-        myapp.controller('timer',['$scope', '$interval', function($scope, $interval){
+        myApp.controller('timer',['$scope', '$interval', function($scope, $interval){
           /*
           ///////////////////////TIMER
           */
-          $scope.starttime = 2000; // 25min in ms
+          $scope.startTime = 2000; // 25min in ms
           $scope.format = 'mm:ss'; //minutes and seconds format 
           var start; //variable holder for start interval 
           
-          $scope.starttimer = function(){
+          $scope.startTimer = function(){
             start = $interval(function(){
-              $scope.starttime -=1000; //starttime is equal to starttime - 1 second
-                if($scope.starttime === 0){// if starttime is = 0 return 25
-                  //$scope.starttime = 1500000; //reset back to 25 min
+              $scope.startTime -=1000; //startTime is equal to startTime - 1 second
+                if($scope.startTime === 0){// if startTime is = 0 return 25
+                  //$scope.startTime = 1500000; //reset back to 25 min
                   $interval.cancel(start); //stop timer at 0
-                  $scope.breaktime = 3000; //set breaktime to 5min
+                  $scope.breakTime = 3000; //set breakTime to 5min
                   $scope.playAlarm(); //play alarm
                   alert("Break time.");
                 }
                 
             },1000);
-          } //end starttimer function
+          } //end startTimer function
           
-          $scope.killtimer = function(starttimer){ //inject starttimer as dependency bc we want to access start
+          $scope.killTimer = function(startTimer){ //inject startTimer as dependency bc we want to access start
             return $interval.cancel(start);// turn off start
           }
           
-          $scope.resettimer = function(starttimer){
+          $scope.resetTimer = function(startTimer){
             $interval.cancel(start); //turn off start aka timer
-            $scope.starttime = 1500000; // set to 25 min
+            $scope.startTime = 1500000; // set to 25 min
           }
           
           /*
            ////////////////////////BREAK TIMER
           */
-          $scope.breaktime = 3000;// 5 min 
-          var breakstart; //variable holder for break start $interval
+          $scope.breakTime = 3000;// 5 min 
+          var breakStart; //variable holder for break start $interval
           
-          $scope.startbreak = function(){
-            breakstart = $interval(function(){
-              $scope.breaktime -=1000; // breaktime is = to breaktime - 1 sec
-              if($scope.breaktime === 0){
-                $interval.cancel(breakstart);
-                $scope.starttime = 2000; //reset timer to 25 min after breaktime === 0
+          $scope.startBreak = function(){
+            breakStart = $interval(function(){
+              $scope.breakTime -=1000; // breakTime is = to breakTime - 1 sec
+              if($scope.breakTime === 0){
+                $interval.cancel(breakStart);
+                $scope.startTime = 2000; //reset timer to 25 min after breakTime === 0
                 $scope.workSesh +=1; // Complete 1 work session
                 $scope.playAlarm(); //play alarm
                 //alert("work session is" + $scope.workSesh);
               }
-            },1000);//end breakstart
-          } //end startbreak function
+            },1000);//end breakStart
+          }; //end startBreak function
           
-          $scope.resetbreak = function(startbreak){ //inject startbreak to access breakstart
-            $interval.cancel(breakstart); //cancel breakstart timer
-            $scope.breaktime = 300000; // set break back to 5min
-          }
+          $scope.resetBreak = function(startBreak){ //inject startBreak to access breakStart
+            $interval.cancel(breakStart); //cancel breakStart timer
+            $scope.breakTime = 300000; // set break back to 5min
+          };
           /*
           /////////////////////WORK SESSIONS + 30 MIN BREAK TIMER
           */
           
           $scope.workSesh = 0; //work session
           
-          $scope.thrtybrk = 5000; //30 min
+          $scope.thrtyBrk = 5000; //30 min
           var thrtybrkStart; // var holder for 30 min break $interval
           
           $scope.startThirtyMinTimer = function(){
             thrtybrkStart = $interval(function(){
-              $scope.thrtybrk -= 1000; // 30 min break is equal to 30 min - 1 sec
-              if($scope.thrtybrk === 0){
+              $scope.thrtyBrk -= 1000; // 30 min break is equal to 30 min - 1 sec
+              if($scope.thrtyBrk === 0){
                 $interval.cancel(thrtybrkStart);//stop 30min break timer
-                $scope.starttime = 2000; //reset timer to 25 min after 30 min break
+                $scope.startTime = 2000; //reset timer to 25 min after 30 min break
                 workSesh = 0; // reset workSesh to 0
                 $scope.playAlarm(); //play alarm
               }
             },1000);
-          }// end 30 min timer
+          };// end 30 min timer
         /*
         ///////////////////////ALARM Player
         */
@@ -86,7 +86,7 @@
           
           $scope.playAlarm = function(){
             return alarm.play();
-          }
+          };
           
         }]);// END OF TIMER CONTROLLER
 
@@ -96,19 +96,19 @@
       /////// FINISHED TASKS CONTROLLER ///////
       ****************************************/
 
-      myapp.controller('finishedTasks',function($scope){
-      $scope.tasks =[
-        {'title':'Go to the gym'}
+      myApp.controller('finishedTasks',function($scope){
+        $scope.tasks =[
+          {'title':'Go to the gym'}
         ];
       
-       $scope.addTask=function(task){
-       $scope.tasks.push({'title':$scope.newtask});
-       $scope.newtask='';
-      }
+        $scope.addTask=function(task){
+        $scope.tasks.push({'title':$scope.newTask});
+        $scope.newTask='';
+        };
       
       $scope.clearTasks = function(){
         $scope.tasks = $scope.tasks.filter(function(task){
           $scope.tasks=[{}];
-        })
-      }
+        });
+      };
     }); ///////// END FINISHED TASKS CONTROLLER 
